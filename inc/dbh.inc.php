3ee1d18fe4bc2fae0bf7b4e2019b4f028e87bfcc
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2021-11-14 17:40:17 +0100

    .

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "fashion";
+$dBName = "cryptolistmining";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
